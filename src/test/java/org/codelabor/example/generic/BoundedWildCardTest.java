458a058041136cb9ab6b50765f0bf35e6a9c777b
codelabor <codelabor@gmail.com>
2017-07-17 16:03:28 +0900

    bounded generic method 추

--- src/test/java/org/codelabor/example/generic/BoundedWildCardTest.java
+++ src/test/java/org/codelabor/example/generic/BoundedWildCardTest.java
@@ -8,6 +8,12 @@ public class BoundedWildCardTest {
 
 	private static final Logger logger = LoggerFactory.getLogger(BoundedWildCardTest.class);
 
+	private <T extends Car> void boundedGenericMethod(WildcardGeneric<T> wildcardGeneric, T item) {
+		wildcardGeneric.setWildcard(item);
+		Car car = wildcardGeneric.getWildcard();
+		logger.info(car.toString());
+	}
+
 	private void boundedWildcardMethod(WildcardGeneric<? extends Car> wildcardGeneric) {
 		Car car = wildcardGeneric.getWildcard();
 		logger.info(car.toString());
@@ -21,10 +27,22 @@ public class BoundedWildCardTest {
 	}
 
 	@Test
+	public void testBus2() {
+		WildcardGeneric<Bus> wildcardGeneric = new WildcardGeneric<Bus>();
+		boundedGenericMethod(wildcardGeneric, new Bus("bus1"));
+	}
+
+	@Test
 	public void testCar() {
 		WildcardGeneric<Car> wildcardGeneric = new WildcardGeneric<Car>();
 		wildcardGeneric.setWildcard(new Car("car1"));
 		boundedWildcardMethod(wildcardGeneric);
+	}
+
+	@Test
+	public void testCar2() {
+		WildcardGeneric<Car> wildcardGeneric = new WildcardGeneric<Car>();
+		boundedGenericMethod(wildcardGeneric, new Car("car1"));
 	}
 
 }
